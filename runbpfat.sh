00df20c86d64dab6fecc9c2ef21969904801519f
ee19b026 <ee19b026@smail.iitm.ac.in>
2022-11-10 19:26:42 +0530

    Ran Simulations

---
+++ runbpfat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 ../Traces/400.perlbench-41B.champsimtrace.xz > ../Results/HashedPerceptron/HashedPerceptron400.txt
+#bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 ../Traces/401.bzip2-226B.champsimtrace.xz > ../Results/HashedPerceptron/HashedPerceptron401.txt
+#bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 ../Traces/403.gcc-16B.champsimtrace.xz > ../Results/HashedPerceptron/HashedPerceptron403.txt
+#bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 ../Traces/410.bwaves-1963B.champsimtrace.xz > ../Results/HashedPerceptron/HashedPerceptron410.txt
+#bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 ../Traces/416.gamess-875B.champsimtrace.xz > ../Results/HashedPerceptron/HashedPerceptron416.txt
+#bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 ../Traces/429.mcf-184B.champsimtrace.xz > ../Results/HashedPerceptron/HashedPerceptron429.txt
+bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 ../Traces/433.milc-127B.champsimtrace.xz  > ../Results/HashedPerceptron/HashedPerceptron433.txt
+bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 ../Traces/435.gromacs-111B.champsimtrace.xz > ../Results/HashedPerceptron/HashedPerceptron435.txt
+bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 ../Traces/437.leslie3d-134B.champsimtrace.xz > ../Results/HashedPerceptron/HashedPerceptron437.txt
+bin/champsim --warmup_instructions 50000000 --simulation_instructions 50000000 ../Traces/444.namd-120B.champsimtrace.xz > ../Results/HashedPerceptron/HashedPerceptron444.txt
